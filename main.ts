786cfba8f6ac39adf90bbeaa4f8c436860c035b1
Nworm <1574242600@qq.com>
2020-07-15 15:22:16 +0800

    Complete class BiliAccount

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ const API_HOST = 'https://api.bilibili.com' ;
 
 enum ApiList {
 	getSelfInfo = '/x/space/myinfo',
-	getFollows = '/x/relation/followings',
-	postFollow = '/x/relation/modify',
+	getFollowsList = '/x/relation/followings',
+	postAddFollow = '/x/relation/modify',
 	getBangumiList = '/x/space/bangumi/follow/list',
 	postAddBangumi = '/pgc/web/follow/add',
 	postVideoHeartBeat = '/x/click-interface/web/heartbeat',
@@ -16,7 +16,7 @@ enum ApiList {
 	getWatchHistory = '/x/web-interface/history/cursor'
 }
 
-interface FollowPostParams {
+interface AddFollowPostParams {
 	fid: number; // 被关注人uid
 	act: number;  // 1 关注  2取关
 	re_src: number;
@@ -32,9 +32,10 @@ interface BangumiListGetParams {
 	vmid: number; // 用户uid
 }
 
-interface GetFollowsParams {
+interface FollowsListGetParams {
 	vmid: number;
 	pn?: number;
+	ps?: number; // 列表长度, 默认且最高50
 	jsonp?: string;
 }
 
@@ -69,9 +70,20 @@ interface CreatedFolderListGetParams {
 interface CreateFolderPostParams {
 	title: string;
 	intro: string;
-	privac: number;	 // 0 公开 1 私人
+	privac: 0 | 1;	 // 0 公开 1 私人
 	cover?: string;
 	csrf: string;
+}
+
+interface FolderVideoListGetParams {
+	media_id: number;
+	pn: number;
+	ps: number;	// 列表长度, 默认20
+	keyword?: string; //搜索
+	order?: string;
+	type?: number;
+	tid?: number;
+	jsonp?: string;
 }
 
 interface WatchHistoryGetParams {
@@ -87,9 +99,6 @@ class BiliAccountCopy {
 	constructor(copy: BiliAccount, to_paste: BiliAccount) {
 		this.copy = copy;
 		this.to_paste = to_paste;
-
-		
-
 	}
 
 	public async run() {
@@ -101,43 +110,202 @@ class BiliAccountCopy {
 class BiliAccount {
 	private cookies: string;
 	private csrf: string;
+	private user_info : object;
 
 	constructor(cookies: string) {
-		try {
-			this.cookies = cookies;
-			this.csrf = cookies.match(/bili_jct=(.*?);/)[1];
-			this.getUserInfo().then(response => {
-				let name : string = response['data']['name'];
-				logger(`用户: ${name}\n`)
-			})
-		} catch (e) {
-			logger('登录失败, 请检查cookies 是否正确')
-			logger(e)
-		}
+		this.cookies = cookies;
+		this.csrf = cookies.match(/bili_jct=(.*?);/)[1];
 	}
 
 	private axios(options: AxiosRequestConfig): Promise<AxiosResponse> {
 		options.headers = {};
+
+		if(options.method == 'POST') {
+			options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+			options.data = this.postDataToString(options.data);
+		}
+
+		options.headers['Referer'] = 'https://www.bilibili.com/';
 		options.headers['Cookie'] = this.cookies;
 		options.baseURL = API_HOST;
-		return axios(options);
+
+	/*
+		options.proxy =  {
+			host: '127.0.0.1',
+			port: 1084,
+		};
+	*/
+		
+		return axios(options).then(response => {
+			if ((response.data)['code'] !== 0) throw '请求错误: \n' + JSON.stringify(response.data);
+			return response;
+		});
+	}
+
+	public async init(): Promise<void> {
+		await this.getUserInfo().then(response => {
+			let name : string = response['name'];
+			if(name === undefined) throw response;
+			this.user_info = response;
+			logger(`用户: ${name}\n`)
+		}).catch(e => {
+			logger('登录失败, 请检查cookies 是否正确')
+			logger(e)
+		})
+	}
+
+	private postDataToString(data: object): string {
+		let arr: string[] = [];
+
+		for (let i in data){
+			arr.push(`${i}=${data[i]}`)
+		}
+
+		return arr.join('&');
 	}
 
 	public async getUserInfo(): Promise<object> {
-		let user_info : object = await this.axios({
+		return await this.axios({
 			method: 'GET',
 			url: ApiList.getSelfInfo,
 		}).then(response => {
-			return response.data;;
+			return (response.data)['data'];
+		});
+	}
+
+	public async getFollowsList(pn: number = 0){
+		let params: FollowsListGetParams = {
+			vmid: this.user_info['mid'],
+			pn: pn
+		}
+
+		return await this.axios({
+			method: 'GET',
+			params: params,
+			url: ApiList.getFollowsList,
+		}).then(response => {
+			return (response.data)['data'];
 		})
-
-		console.log(user_info);
-		return user_info;
 	}
 
-	public async getFollows(pn: number){
+	public async getBangumiList(pn: number = 1): Promise<object> {
+		let params: BangumiListGetParams = {
+			vmid: this.user_info['mid'],
+			type: 1,
+			pn: pn
+		}
 
+		return await this.axios({
+			method: 'GET',
+			params: params,
+			url: ApiList.getBangumiList,
+		}).then(response => {
+			return (response.data)['data'];
+		});
 	}
+
+	public async getCreatedFolderList(): Promise<object> {
+		let params: CreatedFolderListGetParams = {
+			up_mid: this.user_info['mid'],
+			jsonp: 'json'
+		}
+
+		return await this.axios({
+			method: 'GET',
+			params: params,
+			url: ApiList.getCreatedFolderList,
+		}).then(response => {
+			return (response.data)['data'];
+		})
+	}
+
+	public async getFolderVideoList(media_id: number): Promise<object>{
+		let params: FolderVideoListGetParams = {
+			media_id: media_id,
+			pn: 1,
+			ps: 20
+		}
+
+		return await this.axios({
+			method: 'GET',
+			params: params,
+			url: ApiList.getFolderVideoList,
+		}).then(response => {
+			return (response.data)['data'];
+		})
+	}
+
+	public async getWatchHistory(max: number, view_at: number, business: string): Promise<object> {
+		let params: WatchHistoryGetParams = {
+			max: max,
+			view_at: view_at,
+			business: business
+		}
+
+		return await this.axios({
+			method: 'GET',
+			params: params,
+			url: ApiList.getWatchHistory
+		}).then(response => {
+			return (response.data)['data'];
+		});
+	}
+
+	public async postAddFollow(fid: number): Promise<void> {
+		let data: AddFollowPostParams = {
+			fid: fid,
+			act: 1,
+			re_src: 11,
+			csrf: this.csrf,
+		}
+
+		await this.axios({
+			method: 'POST',
+			data: data,
+			url: ApiList.postAddFollow,
+		})
+	}
+
+	public async postAddBangumi(season_id: number): Promise<void> {
+		let data: AddBangumiPostParams = {
+			season_id: season_id,
+			csrf: this.csrf
+		}
+
+		await this.axios({
+			method: 'POST',
+			data: data,
+			url: ApiList.postAddBangumi,
+		})
+	}
+
+	public async postCreateFolder(title: string, intro: string, privac: 0 | 1): Promise<object> {
+		let data: CreateFolderPostParams = {
+			title: title,
+			intro: intro,
+			privac: privac,
+			csrf: this.csrf
+		}
+
+		return await this.axios({
+			method: 'POST',
+			data: data,
+			url: ApiList.postCreateFolder,
+		}).then(response => {
+			return (response.data)['data'];
+		})
+	}
+
+	public async VideoHeartBeatPostParams(data: VideoHeartBeatPostParams): Promise<void> {
+		await this.axios({
+			method: 'POST',
+			data: data,
+			url: ApiList.postCreateFolder,
+		})
+	}
+
+
+	
 }
 
 function logger(msg: string) {
@@ -150,9 +318,13 @@ function logger(msg: string) {
 	
 	let copy_cookies: string = "Cookie: _uuid=3068BFED-3752-2BAC-9320-DC677D9CA4D920118infoc; buvid3=B446E90B-ABC9-4D31-846A-DE3996D7050353930infoc; CURRENT_FNVAL=16; sid=iee9usok; PVID=1; rpdid=|(J~JYR~|u~)0J'ulmku|lYl|; DedeUserID=415983021; DedeUserID__ckMd5=24f94a497395e2c1; SESSDATA=5e96f1c6%2C1609587040%2C8d29b*71; bili_jct=89f221b8fed233287462c81ab85e2731; bp_t_offset_415983021=242601721641167218; bp_video_offset_415983021=411342916458700199; LIVE_BUVID=AUTO4615947037875068"//ReadlineSync.question("需要复制的账户cookies: \n");
 	Account[0] = new BiliAccount(copy_cookies);
+	await Account[0].init()
+
+	//console.log(await Account[0].getWatchHistory(0,0,''));
 
 	let paste_cookies: string = "Cookie: _uuid=3068BFED-3752-2BAC-9320-DC677D9CA4D920118infoc; buvid3=B446E90B-ABC9-4D31-846A-DE3996D7050353930infoc; CURRENT_FNVAL=16; sid=iee9usok; PVID=1; rpdid=|(J~JYR~|u~)0J'ulmku|lYl|; DedeUserID=415983021; DedeUserID__ckMd5=24f94a497395e2c1; SESSDATA=5e96f1c6%2C1609587040%2C8d29b*71; bili_jct=89f221b8fed233287462c81ab85e2731; bp_t_offset_415983021=242601721641167218; bp_video_offset_415983021=411342916458700199; LIVE_BUVID=AUTO4615947037875068"//ReadlineSync.question("需要粘贴的账户cookies: \n");
 	Account[1] = new BiliAccount(paste_cookies);
+	await Account[1].init()
 
 	const Copy : BiliAccountCopy = new BiliAccountCopy(Account[0], Account[1]);
 
